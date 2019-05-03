3ff45ccac094af7bc9a0d399552cc6e134f975d6
lucas-sales <31419535+lucas-sales@users.noreply.github.com>
2019-05-02 21:16:09 -0300

    git ignore

---
+++ controles/Controlador.py
@@ -0,0 +1,6 @@
+from interfaces import InterfaceSensor
+from interfaces import InterfaceBateria
+from interfaces import InterfaceLuz
+from interfaces import InterfacePressao
+from interfaces import InterfaceRotacao
+
